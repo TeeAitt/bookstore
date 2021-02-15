bd79f5eb5d39b8fae60cdef0c247928694af221e
Teemu Aittomäki <teemu.aittomaki@gmail.com>
2021-02-15 17:44:50 +0200

    Added JPA and H2

--- src/main/java/hh/swd20/bookstore/BookstoreApplication.java
+++ src/main/java/hh/swd20/bookstore/BookstoreApplication.java
@@ -1,13 +1,41 @@
 package hh.swd20.bookstore;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import hh.swd20.bookstore.domain.Book;
+import hh.swd20.bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
+	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner demo(BookRepository bookRepository) {  // With this, the database/table contents can be test in console, with the included demo content
+		return (args) -> {
+			log.info("Save some new books, for demo purposes");
+			Book b1 = new Book("Adv", "Jack", 2007, 98765, 19.99);  // You don't need the id attribute, because it's auto generated.
+			Book b2 = new Book("Action", "Peterson", 1988, 94382, 17.99);
+			Book b3 = new Book("Romance", "Nelson", 2013, 97465, 24.99);
+			
+			bookRepository.save(b1);
+			bookRepository.save(b2);
+			bookRepository.save(b3);
+			
+			log.info("Fetch all books");
+			for (Book book : bookRepository.findAll()) {
+				log.info(book.toString());
+			}
+		};
+		
 	}
 
 }
--- src/main/java/hh/swd20/bookstore/domain/Book.java
+++ src/main/java/hh/swd20/bookstore/domain/Book.java
@@ -1,11 +1,25 @@
 package hh.swd20.bookstore.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity		// Describes the structure of the database table that is corresponding to the class
+			// table name, column names and data types, primary key etc.
 public class Book {
+	@Id		// Sets the primary key. It annotates an attribute to be the primary key. In this the attribute is named id (as well).
+	@GeneratedValue(strategy=GenerationType.AUTO)  // Automatically generates new primary key values (in this it's the id), when new information is inputed in the table.
+	private long id;
 	private String title;
 	private String author;
 	private int year;
 	private int isbn;
 	private double price;
+	
+	public Book() {
+		
+	}
 	
 	public Book(String title, String author, int year, int isbn, double price) {
 		super();
@@ -16,50 +30,83 @@ public class Book {
 		this.price = price;
 	}
 
+	
+
+	public long getId() {
+		return id;
+	}
+
+
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+
+
 	public String getTitle() {
 		return title;
 	}
+
+
 
 	public void setTitle(String title) {
 		this.title = title;
 	}
 
+
+
 	public String getAuthor() {
 		return author;
 	}
+
+
 
 	public void setAuthor(String author) {
 		this.author = author;
 	}
 
+
+
 	public int getYear() {
 		return year;
 	}
+
+
 
 	public void setYear(int year) {
 		this.year = year;
 	}
 
+
+
 	public int getIsbn() {
 		return isbn;
 	}
+
+
 
 	public void setIsbn(int isbn) {
 		this.isbn = isbn;
 	}
 
+
+
 	public double getPrice() {
 		return price;
 	}
+
+
 
 	public void setPrice(double price) {
 		this.price = price;
 	}
 
+
 	@Override
 	public String toString() {
-		return "Book [title=" + title + ", author=" + author + ", year=" + year + ", isbn=" + isbn + ", price=" + price
-				+ "]";
+		return "Book [id=" + id + ", title=" + title + ", author=" + author + ", year=" + year + ", isbn=" + isbn
+				+ ", price=" + price + "]";
 	}
 
 }
